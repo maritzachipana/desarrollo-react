7ac44c73287b7810cdb624154849466d841c781f
maritza chipana <chipanam55@gmail.com>
2024-09-14 14:19:48 -0400

    updating package

--- src/index.js
+++ src/index.js
@@ -22,3 +22,21 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { HashRouter as Router } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import store from './store'; // Asegúrate de tener tu store configurado
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <Router>
+//         <App />
+//       </Router>
+//     </Provider>
+//   </React.StrictMode>
+// );
